9be952cc49347a9d30934026486984892f17c0e1
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-07-15 17:04:49 -0300

    creating first src files...

---
+++ rah-cycle/src/cuda-utils.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef __CUDA-UTILS_H__
+#define __CUDA-UTILS_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cuda.h>
+
+static void HandleError(cudaError_t err, const char *file, int line) {
+	if (err != cudaSuccess) {
+		printf("%s in %s at line %d\n", cudaGetErrorString(err), file, line);
+		exit( EXIT_FAILURE);
+	}
+}
+#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+
+/**
+ * This macro checks return value of the CUDA runtime call and exits
+ * the application if the call failed.
+ *
+ * See cuda.h for error code descriptions.
+ */
+#define CHECK_CUDA_RESULT(N) {											\
+	CUresult result = N;												\
+	if (result != 0) {													\
+		printf("CUDA call on line %d returned error %d\n", __LINE__,	\
+			result);													\
+		exit(1);														\
+	} }
+
+#endif //UTILS.H
---
+++ rah-cycle/src/main.cu
@@ -0,0 +1,17 @@
+/**
+ * Copyright 1993-2012 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ */
+#include "cuda-utils.h"
+
+static const int WORK_SIZE = 256;
+
+int main(int argc, char **argv) {
+
+	return 0;
+}
